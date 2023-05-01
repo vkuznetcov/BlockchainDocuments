c47ef6027d4cc8305ef140c9c1dfcea97fee3d7b
slav <vkuznetcov48@gmail.com>
2023-05-01 19:38:36 +0400

    final docBlock page logic

--- src/main/resources/static/js/docBlock.js
+++ src/main/resources/static/js/docBlock.js
@@ -1,63 +1,84 @@
-// window.addEventListener('load', async function () {
-//     if (window.ethereum) {
-//         console.log('Web3 Detected! ' + window.ethereum.constructor.name)
-//         window.web3 = new Web3(window.ethereum);
-//         try {
-//             await window.ethereum.enable()
-//         } catch (error) {
-//             console.error(error)
-//         }
-//
-//     } else {
-//         console.log('No Web3 Detected... using HTTP Provider')
-//         window.web3 = new Web3(new Web3.providers.HttpProvider("https://sepolia.infura.io/v3/304127ae5cee4e06b114cb3d57749335"))
-//     }
-// })
+let transactionHash = ''
 
-function processFile() {
-    return new Promise((resolve, reject) => {
+async function processFile() {
+    if (fileInput.files.length) {
+        const file = fileInput.files[0];
         const reader = new FileReader();
-        const file = document.getElementById('myFile')
-        let hex;
-        let metadata;
+        reader.readAsArrayBuffer(file);
 
-        if (file.files.length) {
-            reader.onload = function (e) {
-                const buffer = e.target.result;
-                hex = web3.utils.soliditySha3(buffer);
-                metadata = file.name + ':' + file.type + ':' + file.size
-                resolve([hex, metadata])
-            };
+        const buffer = await new Promise((resolve, reject) => {
+            reader.addEventListener('load', function () {
+                resolve(reader.result);
+            });
+            reader.addEventListener('error', function () {
+                reject(reader.error);
+            });
+        });
+        const hex = web3.utils.soliditySha3(buffer);
+        const metadata = file.name + ':' + file.type + ':' + file.size;
+        return [hex, metadata];
+    } else {
+        throw new Error('No file selected');
+    }
+}
 
-            reader.readAsBinaryString(file.files[0])
-        } else {
-            reject('No file selected')
+async function sendTransaction(data) {
+    const gasPrice = await web3.eth.getGasPrice();
+    const accountAddress = (await web3.eth.getAccounts())[0];
+
+    console.log('Current gas price: ', web3.utils.fromWei(gasPrice, 'gwei'), ' GWei')
+    console.log('Current commission:', web3.utils.fromWei(gasPrice, 'ether'), ' ETH')
+
+    const transaction = {
+        from: accountAddress,
+        to: docConsts.CONTRACT_ADDRESS,
+        gasPrice: gasPrice,
+        gasLimit: metaMaskConsts.GAS_LIMIT,
+        value: 0x00,
+        data: data
+    }
+
+    const signedTx = await web3.eth.accounts.signTransaction(transaction, metaMaskConsts.METAMASK_PRIVATE_KEY)
+    await web3.eth.sendSignedTransaction(signedTx.rawTransaction, function (error, hash) {
+            if (!error) {
+                console.log("Transaction sent!", hash);
+                const interval = setInterval(function () {
+                    console.log("Attempting to get transaction receipt...");
+
+                    transactionHash = hash
+                    document.getElementById("info").innerHTML
+                        = 'Транзакция еще выполняется <br/> <a href="https://sepolia.etherscan.io/tx/' + hash + '" target="_blank" rel="noopener noreferrer">Показать на etherscan<a/>';
+                    document.getElementById("info").style.display = "block";
+                    document.getElementById("info").style.backgroundColor = "white";
+
+                    web3.eth.getTransactionReceipt(hash, function (err, rec) {
+                        if (rec) {
+                            console.log(rec);
+                            if (rec.status)
+                                document.getElementById("info").innerHTML = "Транзакция выполнена успешно";
+                            else{
+                                document.getElementById("info").innerHTML
+                                    = 'Транзакция не выполнена <br/> <a href="https://sepolia.etherscan.io/tx/' + hash + '" target="_blank" rel="noopener noreferrer">Показать на etherscan<a/>';
+                                document.getElementById("info").style.display = "block";
+                                document.getElementById("info").style.backgroundColor = "rgba(245, 112, 112, 0.12)";
+                            }
+                            clearInterval(interval);
+                        }
+                    });
+                }, 1000);
+            } else {
+                console.log("Something went wrong while submitting your transaction:", error);
+            }
         }
-    })
+    )
 }
 
 async function addDocument() {
     try {
         const [hex, metadata] = await processFile();
-        const gasPrice = await web3.eth.getGasPrice();
-        const accountAddress = (await web3.eth.getAccounts())[0];
-        const contract = new web3.eth.Contract(docConsts.CONTRACT_ABI, docConsts.CONTRACT_ADDRESS);
-
-        console.log('Current gas price: ', web3.utils.fromWei(gasPrice, 'gwei'), ' GWei')
-        console.log('Current commission:', web3.utils.fromWei(gasPrice, 'ether'), ' ETH')
-
-        const transaction = {
-            from: accountAddress,
-            to: docConsts.CONTRACT_ADDRESS,
-            gasPrice: gasPrice,
-            gasLimit: metaMaskConsts.GAS_LIMIT,
-            value: 0x00,
-            data: contract.methods.addDocument(hex, metadata).encodeABI()
-        }
-
-        const signedTx = await web3.eth.accounts.signTransaction(transaction, metaMaskConsts.METAMASK_PRIVATE_KEY)
-        await web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('transactionHash', console.log).on('error', console.error)
-
+        const data = new web3.eth.Contract(docConsts.CONTRACT_ABI, docConsts.CONTRACT_ADDRESS).methods
+            .addDocument(hex, metadata, userId).encodeABI();
+        await sendTransaction(data);
     } catch (error) {
         console.error(error)
     }
@@ -66,25 +87,9 @@ async function addDocument() {
 async function deleteDocument() {
     try {
         const [hex, metadata] = await processFile();
-        const gasPrice = await web3.eth.getGasPrice();
-        const accountAddress = (await web3.eth.getAccounts())[0];
-        const contract = new web3.eth.Contract(docConsts.CONTRACT_ABI, docConsts.CONTRACT_ADDRESS);
-
-        console.log('Current gas price: ', web3.utils.fromWei(gasPrice, 'gwei'), ' GWei')
-        console.log('Current commission:', web3.utils.fromWei(gasPrice, 'ether'), ' ETH')
-
-        const transaction = {
-            from: accountAddress,
-            to: docConsts.CONTRACT_ADDRESS,
-            gasPrice: gasPrice,
-            gasLimit: metaMaskConsts.GAS_LIMIT,
-            value: 0x00,
-            data: contract.methods.deleteDocument(hex).encodeABI()
-        }
-
-        const signedTx = await web3.eth.accounts.signTransaction(transaction, metaMaskConsts.METAMASK_PRIVATE_KEY)
-        await web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('transactionHash', console.log).on('error', console.error)
-
+        const data = new web3.eth.Contract(docConsts.CONTRACT_ABI, docConsts.CONTRACT_ADDRESS).methods
+            .deleteDocument(hex, userId).encodeABI();
+        await sendTransaction(data);
     } catch (error) {
         console.error(error)
     }
@@ -96,10 +101,11 @@ async function verifyDocument() {
         const contract = new web3.eth.Contract(docConsts.CONTRACT_ABI, docConsts.CONTRACT_ADDRESS);
         contract.methods.verifyDocument(hex, metadata).call().then(result => {
                 console.log(result)
+                document.getElementById("info").style.display = "block";
                 result === true ? (
-                    document.getElementById('verifyResult').innerHTML = 'Your document is verified'
+                    document.getElementById('info').innerHTML = 'Ваш файл прошел проверку'
                 ) : (
-                    document.getElementById('verifyResult').innerHTML = 'Your document is not verified'
+                    document.getElementById('info').innerHTML = 'Ваш файл не прошел проверку'
                 )
             }
         )
@@ -108,23 +114,11 @@ async function verifyDocument() {
     }
 }
 
-document.getElementById("addDocument").onclick = async function () {
-    await addDocument()
-}
-
-document.getElementById("verifyDocument").onclick = async function () {
-    await verifyDocument()
-}
-
-document.getElementById("deleteDocument").onclick = async function () {
-    await deleteDocument()
-}
-
-document.getElementById("processDocument").onclick = async function () {
+async function getDocData() {
     const [hex, metadata] = await processFile()
+    let info = document.getElementById("info")
+    info.innerHTML = "Хэш: " + hex.substring(0, 13) + "... <br/> Метаданные: " + metadata
+    info.style.display = "block"
+    console.log(hex)
     console.log(metadata)
-}
-
-document.getElementById("myFile").onchange = async function () {
-    await verifyDocument()
 }
